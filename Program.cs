f4549cfb1bf16505365f40fa9f175bde8ff48895
Elmin Huseyinli <tu7d5w0m5@aztu.local>
2022-03-18 19:25:11 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Menu\n1-Send message by email\n2-Send message by sms\n0-Quit");
+            string CommandStr = Console.ReadLine();          
+            switch (CommandStr)
+            {
+                case "1":
+                    EmailSender emailSender = new EmailSender();
+                    emailSender.SendMessage();
+                    break; 
+                case "2" :
+                    SmsSender smsSender = new SmsSender();
+                    smsSender.SendMessage();
+                    break;
+                case "0":
+                    Console.WriteLine("Quit");
+                    break;
+                default:
+                    Console.WriteLine("Wrong Command");
+                    break; 
+            }
+        }
+    }
+}
